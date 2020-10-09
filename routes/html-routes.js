3a4a3316bbc580332ce3a3bf49bc562a5ba04154
Haley Myers <haleynmyers@gmail.com>
2020-10-08 19:14:25 -0500

    Added html routes

---
+++ routes/html-routes.js
@@ -0,0 +1,16 @@
+const app = require("express").Router();
+const path = require("path");
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(_dirname, "../public/stats.html"));
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(_dirname, "../public/index.html"));
+});
+
+module.exports = app;
